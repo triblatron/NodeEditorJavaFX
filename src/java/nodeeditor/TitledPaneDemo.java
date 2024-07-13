4e7d32398dc96c03f963db3d2ff89d9c67cb6ab4
Tony Horrobin <tony.horrobin@gmail.com>
2024-07-13 18:38:55 +0100

    feat:Add demo of a TitledPane to show collapsible content.

---
+++ src/java/nodeeditor/TitledPaneDemo.java
@@ -0,0 +1,30 @@
+package nodeeditor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
+
+public class TitledPaneDemo extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/nodeeditor/titledpane.fxml"));
+            //    Parent root = FXMLLoader.load(getClass().getResource("view/Calculatorview.fxml"));
+            TitledPane root = (TitledPane) loader.load();
+            Scene scene = new Scene(root);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch(Exception e)    {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
